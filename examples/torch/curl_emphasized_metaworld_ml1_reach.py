87f95c2ead6efd2dc5e991a69725e8a8c269ea3c
simon0xzx <simon0xzx@gmail.com>
2020-08-11 00:23:22 -0700

    adding emphasized architecture

--- examples/torch/curl_emphasized_metaworld_ml1_reach.py
+++ examples/torch/curl_emphasized_metaworld_ml1_reach.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""MULTITASKORACLE ML1 example."""
+"""PEARL ML1 example."""
 import click
 import metaworld.benchmarks as mwb
 
@@ -10,9 +10,10 @@ from garage.experiment.deterministic import set_seed
 from garage.experiment.task_sampler import SetTaskSampler
 from garage.sampler import LocalSampler
 from garage.torch import set_gpu_mode
-from garage.torch.algos import MULTITASKORACLE
-from garage.torch.algos.multi_task_oracle import MULTITASKORACLEWorker
-from garage.torch.policies import GoalConditionedPolicy
+from garage.torch.algos import CURL
+from garage.torch.algos.curl import CURLWorker
+from garage.torch.embeddings import ContrastiveEncoder
+from garage.torch.policies import CurlPolicy
 from garage.torch.policies import TanhGaussianContextEmphasizedPolicy
 from garage.torch.q_functions import ContinuousMLPQFunction
 
@@ -31,18 +32,18 @@ from garage.torch.q_functions import ContinuousMLPQFunction
 @click.option('--embedding_batch_size', default=64)
 @click.option('--embedding_mini_batch_size', default=64)
 @click.option('--max_path_length', default=150)
-@click.option('--seed', default=1)
 @click.option('--gpu_id', default=0)
 @wrap_experiment
-def curl_emphasized_oracle_metaworld_ml1_pick_place(ctxt=None,
+def curl_emphasized_metaworld_ml1_reach(ctxt=None,
                              seed=1,
-                             num_epochs=500,
+                             num_epochs=1000,
                              num_train_tasks=50,
                              num_test_tasks=10,
+                             latent_size=7,
                              encoder_hidden_size=200,
                              net_size=300,
-                             meta_batch_size=128,
-                             num_steps_per_epoch=500,
+                             meta_batch_size=16,
+                             num_steps_per_epoch=4000,
                              num_initial_steps=4000,
                              num_tasks_sample=15,
                              num_steps_prior=750,
@@ -52,9 +53,9 @@ def curl_emphasized_oracle_metaworld_ml1_pick_place(ctxt=None,
                              embedding_mini_batch_size=64,
                              max_path_length=150,
                              reward_scale=10.,
-                             use_gpu=True,
-                             gpu_id = 0):
-    """Train MULTITASKORACLE with ML1 environments.
+                             gpu_id = 0,
+                             use_gpu=True):
+    """Train PEARL with ML1 environments.
 
     Args:
         ctxt (garage.experiment.ExperimentContext): The experiment
@@ -91,38 +92,41 @@ def curl_emphasized_oracle_metaworld_ml1_pick_place(ctxt=None,
 
     """
     set_seed(seed)
+    encoder_hidden_sizes = (encoder_hidden_size, encoder_hidden_size,
+                            encoder_hidden_size)
     # create multi-task environment and sample tasks
-    train_env = GarageEnv(normalize(mwb.ML1.get_train_tasks('pick-place-v1')))
-    env_sampler = SetTaskSampler(lambda: train_env)
-    env = env_sampler.sample_with_goals(num_train_tasks)
+    env_sampler = SetTaskSampler(lambda: GarageEnv(
+        normalize(mwb.ML1.get_train_tasks('reach-v1'))))
+    env = env_sampler.sample(num_train_tasks)
 
-    test_env = GarageEnv(normalize(mwb.ML1.get_test_tasks('pick-place-v1')))
-    test_env_sampler = SetTaskSampler(lambda: test_env)
-
+    test_env_sampler = SetTaskSampler(lambda: GarageEnv(
+        normalize(mwb.ML1.get_test_tasks('reach-v1'))))
     runner = LocalRunner(ctxt)
 
     # instantiate networks
-    latent_size = 3 # (NEW) 3-Dimensional context variable addressing task goal
-    augmented_env = MULTITASKORACLE.augment_env_spec(env[0](), latent_size)
+    augmented_env = CURL.augment_env_spec(env[0](), latent_size)
     qf = ContinuousMLPQFunction(env_spec=augmented_env,
                                 hidden_sizes=[net_size, net_size, net_size])
 
-    vf_env = MULTITASKORACLE.get_env_spec(env[0](), latent_size, 'vf')
+    vf_env = CURL.get_env_spec(env[0](), latent_size, 'vf')
     vf = ContinuousMLPQFunction(env_spec=vf_env,
                                 hidden_sizes=[net_size, net_size, net_size])
 
     inner_policy = TanhGaussianContextEmphasizedPolicy(
-        env_spec=augmented_env, hidden_sizes=[net_size, net_size, net_size], latent_sizes=3)
+        env_spec=augmented_env, hidden_sizes=[net_size, net_size, net_size],
+        latent_sizes=latent_size)
 
-    multitask_oracle = MULTITASKORACLE(
+    curl = CURL(
         env=env,
-        policy_class=GoalConditionedPolicy,
+        policy_class=CurlPolicy,
+        encoder_class=ContrastiveEncoder,
         inner_policy=inner_policy,
         qf=qf,
         vf=vf,
         num_train_tasks=num_train_tasks,
         num_test_tasks=num_test_tasks,
         latent_dim=latent_size,
+        encoder_hidden_sizes=encoder_hidden_sizes,
         test_env_sampler=test_env_sampler,
         meta_batch_size=meta_batch_size,
         num_steps_per_epoch=num_steps_per_epoch,
@@ -135,21 +139,19 @@ def curl_emphasized_oracle_metaworld_ml1_pick_place(ctxt=None,
         embedding_mini_batch_size=embedding_mini_batch_size,
         max_path_length=max_path_length,
         reward_scale=reward_scale,
-        replay_buffer_size=200000
     )
 
     set_gpu_mode(use_gpu, gpu_id=gpu_id)
     if use_gpu:
-        multitask_oracle.to()
+        curl.to()
 
-    runner.setup(algo=multitask_oracle,
+    runner.setup(algo=curl,
                  env=env[0](),
                  sampler_cls=LocalSampler,
                  sampler_args=dict(max_path_length=max_path_length),
                  n_workers=1,
-                 worker_class=MULTITASKORACLEWorker)
+                 worker_class=CURLWorker)
 
     runner.train(n_epochs=num_epochs, batch_size=batch_size)
 
-
-curl_emphasized_oracle_metaworld_ml1_pick_place()
+curl_emphasized_metaworld_ml1_reach()
