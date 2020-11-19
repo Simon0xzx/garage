bd48f7e633ac354ba84f56a623e1d453e53820f1
simon0xzx <simon0xzx@gmail.com>
2020-11-19 02:07:15 -0800

    correct parameters

--- examples/simon_paper/curl_paper_ml1.py
+++ examples/simon_paper/curl_paper_ml1.py
@@ -21,13 +21,13 @@ from garage.torch.q_functions import ContinuousMLPQFunction
 @click.command()
 @click.option('--num_epochs', default=100)
 @click.option('--seed', default=1)
-@click.option('--num_train_tasks', default=5)   # TODO here 50
-@click.option('--num_test_tasks', default=2)    # TODO here 10
+@click.option('--num_train_tasks', default=50)   # TODO here 50
+@click.option('--num_test_tasks', default=10)    # TODO here 10
 @click.option('--latent_size', default=7)
 @click.option('--encoder_hidden_size', default=400)
 @click.option('--net_size', default=400)
-@click.option('--num_steps_per_epoch', default=400)    # number of train per iter # TODO herev 4000
-@click.option('--num_initial_steps', default=400)      # TODO here 4000
+@click.option('--num_steps_per_epoch', default=4000)    # number of train per iter TODO here 4000
+@click.option('--num_initial_steps', default=4000)      # TODO here 4000
 @click.option('--num_steps_prior', default=750)
 @click.option('--num_extra_rl_steps_posterior', default=750)
 @click.option('--batch_size', default=256)
