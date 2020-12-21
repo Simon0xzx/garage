a3bf5856d87b7bd0804a5d962767b3b2c67a6e86
simon0xzx <simon0xzx@gmail.com>
2020-12-21 15:54:10 -0800

    Adding skeletons

---
+++ tests/integration_tests/simon_display_helper.py
@@ -0,0 +1,86 @@
+import numpy as np
+from collections import defaultdict
+import os
+import csv
+"""
+#########################################################
+##                       Ploting                       ##
+#########################################################
+"""
+
+def read_csv_file(file_path, type=float):
+    verify_key = 'MetaTest/Average/AverageReturn'
+    data_dict = defaultdict(lambda: [])
+    with open(file_path, newline='\n') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            for k, v in row.items():
+                try:
+                    if v == '':
+                        data_dict[k].append(0.0)
+                    else:
+                        data_dict[k].append(type(v))
+                except:
+                    print("problem: {}".format(v))
+    delete_index = []
+    return data_dict
+
+def plot_curve_avg(matplot, exps, format='-',
+                   title = 'MetaTest/Average/AverageReturn', x_title = 'TotalEnvSteps',
+                   legend = None, limit = -1, line_width=2, opacity=0.2, env_step_limit=2500000, backward_smooth_window=1):
+    if exps == None or len(exps) == 0:
+        return
+    result_paths = [os.path.join(name, 'progress.csv') for name in exps]
+    data_dicts = [read_csv_file(result_path) for result_path in result_paths]
+    min_step_length = min([len(data_dict[x_title]) for data_dict in data_dicts])
+    if limit != -1:
+        min_step_length = min(min_step_length, limit)
+    label = legend if legend != None else'{}_{}'.format(result_paths[0], title)
+    x = data_dicts[0][x_title][:min_step_length]
+    y = [list(map(lambda x: float(x), data_dict[title][:min_step_length])) for data_dict in data_dicts]
+    y_ave = np.average(y, axis=0)
+    y_min = np.min(y, axis=0)
+    y_max = np.max(y, axis=0)
+
+    x_new, y_avg_new, y_min_new, y_max_new = [],[],[],[]
+    last_visited_index = 0
+    for i in range(len(x)):
+        if x[i] > env_step_limit:
+            break
+        last_visited_index = i
+        x_new.append(x[i])
+        y_avg_new.append(y_ave[i])
+        y_min_new.append(y_min[i])
+        y_max_new.append(y_max[i])
+
+    final_y_avg = np.average(y_ave[last_visited_index-5: last_visited_index])
+    final_y_std = np.std(y_ave[last_visited_index-5: last_visited_index])
+    print(exps[0])
+    print("Last 10 Step average: {}".format(final_y_avg))
+    print("Last 10 Step std: {}".format(final_y_std))
+    print("Y Last Min: {}".format(y_min_new[-1]))
+    print("Y Last Avg: {}".format(y_avg_new[-1]))
+    print("Y Last Max: {}".format(y_max_new[-1]))
+    matplot.plot(x_new, y_avg_new, format, lw=line_width,label=label)
+    matplot.fill_between(x_new, y_min_new, y_max_new, alpha=opacity)
+    matplot.legend()
+
+def print_hyper_tests(axs, dir_path, exp_name, label, num_seeds=1,
+                      title = 'MetaTest/Average/AverageReturn', x_title = 'TotalEnvSteps',
+                      limit = -1, env_step_limit=2500000, backward_smooth_window=1):
+    results_dirs = []
+    for i in range(num_seeds):
+        task_name = '{}{}'.format(exp_name, '_{}'.format(i) if i > 0 else '')
+        if 'meta-q-learning' in dir_path:
+            direct_dir_path = '{}/metaworld-ml1-{}_mql_dummy'.format(dir_path, task_name)
+            title = 'episode_reward'
+            x_title = 'total_timesteps'
+        else:
+            direct_dir_path = '{}/{}'.format(dir_path, task_name)
+        if os.path.exists(direct_dir_path):
+            results_dirs.append(direct_dir_path)
+    try:
+        plot_curve_avg(axs, results_dirs, '-',
+                       legend=label, title=title, x_title=x_title, limit=limit, env_step_limit=env_step_limit, backward_smooth_window=backward_smooth_window)
+    except:
+        print("Failed to Locate {}".format(exp_name))
---
+++ tests/integration_tests/simon_report.py
@@ -0,0 +1,183 @@
+from tests.integration_tests.simon_display_helper import print_hyper_tests, read_csv_file
+import matplotlib.pyplot as plt
+from os import path
+
+# Name -> (exp_root_path, labels)
+def get_data_repos():
+    data_dir = '/home/simon0xzx/research/berkely_research/garage/data'
+    data_repo = {
+    'pearl_old_ml1_result': (path.join(data_dir, '/namazu/ml1_results'), 'old_pearl_result'),
+    'curl_old_ml1_result': ('/home/simon0xzx/research/berkely_research/garage/data/namazu/ml1_results', 'old_curl_result'),
+    'large_batch': ('/home/simon0xzx/research/berkely_research/garage/data/namazu/curl_test2', 'curl_large_batch_size'),
+    'new_label': ('/home/simon0xzx/research/berkely_research/garage/data/local/curl_new_label', 'curl_new_label_batch_16', 'curl_new_label_batch_64'),
+    'namazu_new_label': ('/home/simon0xzx/research/berkely_research/garage/data/namazu/curl_new_label', 'curl_new_label_batch_32'),
+    # full suit results
+    'old_curl': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/old_curl_ml1_result_suit', 'old_curl'),
+    'pearl': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/pearl', 'pearl'),
+    'curl_labeled_b16': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_labeled_b16', 'curl_labeled_b16'),
+    'updated_curl': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_normal', 'updated_curl'),
+    'classifier_encoder':('/home/simon0xzx/research/berkely_research/garage/data/local/classifier_suit', 'classifier_encoder'),
+    'curl_wasserstein_old': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_wasserstein', 'curl_wasserstein_old'),
+    'curl_wasserstein': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_wasserstein_final', 'curl_wasserstein'),
+    'curl_wasserstein_2_step': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_wasserstein_final_2_steps', 'curl_wasserstein_2_step'),
+    'curl_wasserstein_2': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_wasserstein_final2', 'curl_wasserstein'),
+    'rl2_ppo_suit': ('/home/simon0xzx/research/berkely_research/garage/data/result_suits/rl2_ppo_suit', 'rl2_ppo'),
+    # 10 envs
+    'curl_wasserstein_env_step_400': ("/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_wasserstein_env_steps_400", "curl_wasserstein_env_steps_400"),
+    'curl_wasserstein_env_step_2000':("/home/simon0xzx/research/berkely_research/garage/data/result_suits/curl_wasserstein_env_steps_2000", "curl_wasserstein_env_steps_2000"),
+    'curl_wasserstein_long_10': (path.join(data_dir, 'result_suits/curl_wasserstein_long_10_suit'),'curl_wasserstein'),
+    'pearl_long_10': (path.join(data_dir, 'result_suits/pearl_10_suit'), 'pearl'),
+
+    # other methods
+    'maml_trpo': ('/home/simon0xzx/research/berkely_research/garage/data/local/maml_trpo_suit_2','maml_trpo'),
+    'maml_ppo': ('/home/simon0xzx/research/berkely_research/garage/data/local/maml_ppo_suit', 'maml_ppo'),
+    'rl2_ppo': ('/home/simon0xzx/research/berkely_research/garage/data/local/rl2_ppo_suit', 'rl2_ppo'),
+    'rl2_ppo_2': ('/home/simon0xzx/research/berkely_research/garage/data/local/rl2_ppo_suit_2', 'rl2_ppo'),
+    'rl2_ppo_larger_net': ('/home/simon0xzx/research/berkely_research/garage/data/local/rl2_ppo_larger_net_suit', 'rl2_ppo_larger_net'),
+    'rl2': ('/home/simon0xzx/research/berkely_research/garage/data/namazu/rl2_suit', 'rl2'),
+    'mql': ('/home/simon0xzx/research/berkely_research/meta-q-learning/log_dir', 'mql'),
+
+    # local stuff
+    'curl_w_env_step': ('/home/simon0xzx/research/berkely_research/garage/data/local/curl_wasserstein_env_steps', 'env_steps')
+
+    }
+    return data_repo
+
+def get_metaworld_task_list(task_portion='Odd'):
+    task_name_csv = '/home/simon0xzx/research/berkely_research/garage/examples/metaworld_exp_name.txt'
+    csv_dict = read_csv_file(task_name_csv, type=str)
+    if task_portion == 'Odd':
+        task_names = [task_name for i, task_name in enumerate(csv_dict['task_name']) if i%2 ==0]
+    elif task_portion == 'Even':
+        task_names = [task_name for i, task_name in enumerate(csv_dict['task_name']) if i % 2 == 1]
+    else:
+        task_names = csv_dict['task_name']
+
+    return task_names
+
+
+
+def metaworld_ml1_graph(axs, task_lists, draw_repo_names, row=4, col=6, num_seeds=1,
+                        limit=-1, title='MetaTest/Average/AverageReturn', x_title='TotalEnvSteps',
+                        env_step_limit=2500000, backward_smooth_window=1):
+    data_repo = get_data_repos()
+    for i, task in enumerate(task_lists):
+        row_cnt = int(i / col)
+        col_cnt = i % col if i % col >= 0 else (i % col) + col
+        if col == 1 and row > 1:
+            subplot_axs = axs[row_cnt]
+        elif row == 1 and col > 1:
+            subplot_axs = axs[col_cnt]
+        elif row == 1 and col ==1:
+            subplot_axs = axs
+        else:
+            subplot_axs = axs[row_cnt][col_cnt]
+        subplot_axs.set_title('ML1 {}'.format(task))
+        subplot_axs.set_xlabel(x_title)
+        subplot_axs.set_ylabel(title.split('/')[-1])
+        subplot_axs.ticklabel_format(useMathText=True)
+        subplot_axs.axvline(x=env_step_limit, linewidth=2, color='r')
+        # subplot_axs.set_xlim(0, env_step_limit)
+        for repo in draw_repo_names:
+            exp_repo = data_repo[repo]
+            print_hyper_tests(subplot_axs, exp_repo[0], task, exp_repo[1],
+                              num_seeds = num_seeds, title=title, x_title=x_title, limit=limit, env_step_limit=env_step_limit, backward_smooth_window=backward_smooth_window)
+    plt.show()
+
+
+def plot_full_suits():
+    sampled_task_lists = ['faucet-close-v1', 'soccer-v1', 'plate-slide-side-v1', 'stick-push-v1']
+    full_suit_task_lists = ['faucet-open-v1', 'faucet-close-v1', 'lever-pull-v1', 'stick-push-v1', 'handle-pull-side-v1', 'stick-pull-v1', 'dissassemble-v1', 'coffee-push-v1', 'hammer-v1', 'plate-slide-side-v1', 'handle-press-v1', 'soccer-v1', 'plate-slide-back-v1', 'button-press-topdown-v1', 'button-press-topdown-wall-v1', 'peg-insert-side-v1', 'push-wall-v1', 'button-press-v1', 'coffee-pull-v1', 'window-close-v1', 'door-open-v1', 'drawer-open-v1', 'box-close-v1', 'door-unlock-v1', 'basketball-v1']
+    row, col = 5,5
+    fig, axs = plt.subplots(row, col)
+    # plt.subplots_adjust(left=0.05, bottom=0.05, right=0.99, top=0.96,
+    #                     wspace=0.26, hspace=0.70)
+    # row, col = 4, 6
+    plt.subplots_adjust(left=0.04, bottom=0.04, right=0.98, top=0.96,
+                        wspace=0.25, hspace=0.30)
+
+    # metaworld_ml1_graph(axs, full_suit_task_lists, ['pearl', 'curl_wasserstein', 'curl_wasserstein_2_step'], row=row, col=col)
+    # metaworld_ml1_graph(axs, full_suit_task_lists, ['old_curl', 'updated_curl', 'curl_wasserstein', 'curl_wasserstein_2_step'], row=row, col=col)
+    metaworld_ml1_graph(axs, full_suit_task_lists, ['curl_wasserstein_2', 'rl2_ppo_suit', 'pearl'], num_seeds=3,  row=row, col=col, limit=100)
+
+    plt.show()
+
+'''
+soccer-v1
+push-wall-v1
+coffee-pull-v1
+button-press-topdown-wall-v1
+faucet-close-v1
+pick-out-of-hole-v1
+dial-turn-v1
+plate-slide-back-side-v1
+window-open-v1
+door-close-v1
+'''
+def plot_selected_suits():
+
+    sampled_task_lists = ['soccer-v1', 'push-wall-v1', 'coffee-pull-v1',
+                          'button-press-topdown-wall-v1', 'faucet-close-v1',
+                          'pick-out-of-hole-v1', 'dial-turn-v1', 'plate-slide-back-side-v1',
+                          'window-open-v1', 'door-close-v1']
+
+    row, col = 2,5
+    fig, axs = plt.subplots(row, col)
+    plt.subplots_adjust(left=0.05, bottom=0.05, right=0.99, top=0.96,
+                        wspace=0.26, hspace=0.70)
+    # row, col = 4, 6
+    # plt.subplots_adjust(left=0.04, bottom=0.04, right=0.98, top=0.96,
+    #                     wspace=0.25, hspace=0.30)
+
+    # metaworld_ml1_graph(axs, full_suit_task_lists, ['pearl', 'curl_wasserstein', 'curl_wasserstein_2_step'], row=row, col=col)
+    metaworld_ml1_graph(axs, sampled_task_lists, ['curl_wasserstein_long_10', 'pearl_long_10', 'rl2_ppo_2'], limit=100, num_seeds=3, row=row, col=col)
+
+    # , 'pearl_long_10', 'pearl'
+    plt.show()
+
+
+
+def varify():
+    sampled_task_lists = ['soccer-v1', 'drawer-open-v1', 'button-press-topdown-v1']
+    full_suit_task_lists = ['faucet-open-v1', 'faucet-close-v1',
+                            'lever-pull-v1', 'stick-push-v1',
+                            'handle-pull-side-v1', 'stick-pull-v1',
+                            'dissassemble-v1', 'coffee-push-v1', 'hammer-v1',
+                            'plate-slide-side-v1', 'handle-press-v1',
+                            'soccer-v1', 'plate-slide-back-v1',
+                            'button-press-topdown-v1',
+                            'button-press-topdown-wall-v1',
+                            'peg-insert-side-v1', 'push-wall-v1',
+                            'button-press-v1', 'coffee-pull-v1',
+                            'window-close-v1', 'door-open-v1',
+                            'drawer-open-v1', 'box-close-v1', 'door-unlock-v1', 'basketball-v1']
+
+    row, col = 5, 5
+    fig, axs = plt.subplots(row, col)
+    plt.subplots_adjust(left=0.05, bottom=0.05, right=0.99, top=0.96,
+                        wspace=0.20, hspace=0.40)
+    metaworld_ml1_graph(axs, full_suit_task_lists, [ 'curl_wasserstein_2', 'curl_wasserstein_env_step_2000',  'curl_wasserstein_env_step_400'], num_seeds=3, row=row, col=col, limit=100, env_step_limit=2500000)
+
+
+def process():
+    stuff = ""
+
+    for line in stuff.split('\n'):
+        env = line.split(':')[0]
+        statement = "sudo tmux kill-session -t {}".format(env)
+        print(statement)
+
+def simple_draw():
+    sampled_task_lists = ['handle-press-v1', 'handle-press-v1_1', 'handle-press-v1_2', 'handle-press-v1_3']
+    row, col = 2, 2
+    fig, axs = plt.subplots(row, col)
+    metaworld_ml1_graph(axs, sampled_task_lists,
+                        ['curl_w_env_step'], num_seeds=1,
+                        row=row, col=col, limit=100)
+
+if __name__ == '__main__':
+#     # plot_full_suits()
+#     # plot_selected_suits()
+    varify()
+    # process()
+    # simple_draw()
